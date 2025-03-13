a12fa4fd633b44cfedd4a0b8d1ca97a4c0f02d61
Cristian Zapata Arias <crizapat@c1r12s3.42madrid.com>
2025-03-13 17:08:12 +0100

    feat(chore): practice

--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,18 @@
 /*   By: crizapat <crizapat@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 16:28:39 by crizapat          #+#    #+#             */
-/*   Updated: 2025/03/06 17:16:44 by crizapat         ###   ########.fr       */
+/*   Updated: 2025/03/13 16:20:17 by crizapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include "./libft/libft.h"
 # include  <unistd.h>
 # include  <stdio.h>
 
 typedef	int	t_bool;
 #define TRUE 0
 #define FALSE 1
-
 
 #endif 
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: crizapat <crizapat@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 16:30:40 by crizapat          #+#    #+#             */
-/*   Updated: 2025/03/06 16:39:46 by crizapat         ###   ########.fr       */
+/*   Updated: 2025/03/13 17:07:42 by crizapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int main(int argc, char **argv)
 {
+	int i;	
+
+	if(argc == 1)
+	{
+		printf("Not enough parameters\n");
+		exit(EXIT_FAILURE);
+	}
+	else {
+		i = 0;
+		while(i < argc)
+		{
+			printf("Argument: %d, position %d\n", ft_atoi(argv[i]), i);
+			i++;
+		}
+	}
 	return (0);
 }
