b179eaad0a576bebfbe669a02e1f0e54235c741b
Cristian Zapata Arias <crizapat@c2r11s6.42madrid.com>
2025-04-15 11:12:53 +0200

    feat(src): upload indexer to sort nodes

--- src/push_swap.c
+++ src/push_swap.c
@@ -34,7 +34,33 @@ t_bool fill_stack(t_stack *stack_a, int argc, char **argv)
 	return (TRUE);
 }
 
+t_bool indexer(t_stack *stack_a)
+{
+	t_node	*curren_node;	
+	t_node	*node_to_compare;	
+	size_t	index;
 
+	if (!stack_a->head || !stack_a->size)
+		return (FALSE);
+
+	curren_node = stack_a->head;	
+	while (curren_node)
+	{
+		index = 0;
+		node_to_compare = stack_a->head; 
+		while (node_to_compare)
+		{
+			if (node_to_compare->number < curren_node->number)
+				index++;
+			node_to_compare = node_to_compare->next;
+		}
+		curren_node->index = index;
+		curren_node = curren_node->next;
+	}
+	// WIP verify this implementation
+	// stack_a->size = index + 1;	
+	return (TRUE);
+}
 
 int main(int argc, char **argv)
 {
