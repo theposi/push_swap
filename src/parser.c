c936fd4f4bf4bfbc7de52415900bc4816df9e4a0
Cristian Zapata <theposidesign@gmail.com>
2025-04-29 16:25:01 +0200

    feat(src): add repeat numbers checker

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: crizapat <crizapat@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 16:27:38 by crizapat          #+#    #+#             */
-/*   Updated: 2025/04/11 12:33:33 by crizapat         ###   ########.fr       */
+/*   Updated: 2025/04/29 12:11:09 by crizapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,24 @@ t_bool	args_checker(int argc, char **argv)
 	}
 	return (TRUE);
 }
+
+t_bool check_duplicate(t_stack stack_a)
+{
+	t_node *node_iterator;
+	t_node *temp_node;
+
+	node_iterator = stack_a.head;
+	while (node_iterator != NULL && node_iterator->next != NULL)
+	{
+		temp_node = node_iterator;
+		while (temp_node != NULL)
+		{
+			if (node_iterator->number == temp_node->number)
+				return (FALSE);
+			temp_node = temp_node->next;
+		}
+		node_iterator = node_iterator->next;
+	}
+	return (TRUE);
+}
+
